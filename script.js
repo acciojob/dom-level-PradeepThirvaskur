ea26a7d11419465057e59b7f103b0c0c7399169e
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-11-13 17:58:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+  let levelElement = document.querySelector("#level");
+let counter = 0;
+
+while (levelElement.parentElement) {
+  levelElement = levelElement.parentElement;
+  counter++; 
+}
+
+alert(`The level of the element is: ${counter}`);
